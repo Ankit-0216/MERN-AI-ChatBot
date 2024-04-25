aab857cf292c9eb3e3feb4f6d558f7e404ba5188
Ankit-0216 <ankitsethi1602@gmail.com>
2024-04-25 10:54:40 +0530

    added User and Chat schema

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,3 +1,28 @@
-export const getAllUsers = () => {
-  // get all users
+import { Request, Response } from "express";
+import User from "../models/User.js";
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+
+    if (users.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No users Found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User details fetched Successfully",
+      users,
+    });
+  } catch (error) {
+    console.log("Erron in getAllUsers", error);
+    return res.status(500).json({
+      success: false,
+      message: "Unable to get all users.",
+      error: error.message,
+    });
+  }
 };
